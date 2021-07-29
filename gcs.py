ad5f5c7dce48fc02c69a61255fefcb35be6b2276
impactWhey-KSW <plackyou123@korea.ac.kr>
2021-07-29 11:39:23 +0900

    gcs , folder modify

--- gcs.py
+++ gcs.py
@@ -9,11 +9,11 @@ import io
 import socket
 import pandas as pd 
 import serial
+
 class window(QtWidgets.QMainWindow):
     def __init__(self):
         QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.PluginsEnabled,True)
         super(window,self).__init__()
-        #self.setGeometry(100,100,1900,1000)
         self.centralwid=QtWidgets.QWidget(self)
         self.vlayout_main=QtWidgets.QVBoxLayout()
         self.btnSerial = QtWidgets.QPushButton('connect : STM32',self) ; self.btnSocket = QtWidgets.QPushButton('connect : NX',self)
@@ -31,7 +31,20 @@ class window(QtWidgets.QMainWindow):
         self.m.save(self.data, close_file=False)
         self.statuslayout = QtWidgets.QHBoxLayout()
         self.altstatus = QtWidgets.QLabel("고도 : ",self)
+        self.mission_lat = QtWidgets.QLineEdit("Mission lat",self)
+        self.mission_lon = QtWidgets.QLineEdit("Mission lon",self)
+        self.RTH_lat = QtWidgets.QLineEdit("RTH lat",self)
+        self.RTH_lon = QtWidgets.QLineEdit("RTH lon",self)
+        self.mission_lat.returnPressed.connect(self.missionlatFunction)
+        self.mission_lon.returnPressed.connect(self.missionlonFunction)
+        self.RTH_lat.returnPressed.connect(self.RTHlatFunction)
+        self.RTH_lon.returnPressed.connect(self.RTHlonFunction)
         self.statuslayout.addWidget(self.altstatus)
+        self.statuslayout.addWidget(self.mission_lat)
+        self.statuslayout.addWidget(self.mission_lon)
+        self.statuslayout.addWidget(self.RTH_lat)
+        self.statuslayout.addWidget(self.RTH_lon)
+        
         self.webview2=QtWebEngineWidgets.QWebEngineView()
         self.webview2.setHtml(self.data.getvalue().decode())
         self.weblayout = self.btnlayout=QtWidgets.QHBoxLayout()
@@ -49,11 +62,11 @@ class window(QtWidgets.QMainWindow):
         self.show()
         self.timer = QTimer(self)
         self.timer.start(1000)
-        self.rad = 1 
+        self.rad = 1
         self.HOST = '192.168.43.185'
         #self.HOST = '127.0.0.1'
         self.port = 9999
-        self.lat_drone = [] ;self.lon_drone = [] ;self.lat_person = [] ;self.lon_person = [] ;self.altitude = []
+        self.lat_drone = [] ;self.lon_drone = [] ; self.GPStime = [] ; self.lat_person = [] ;self.lon_person = [] ;self.altitude = []
         self.server_socket = 0 ; self.client_socket = 0 ; self.addr = 0;
         self.NX_data = b'0'
         self.btnSerial.clicked.connect(self.connectSerial) ; self.btnSocket.clicked.connect(self.connectSocket)
@@ -63,67 +76,90 @@ class window(QtWidgets.QMainWindow):
         self.header_1 = b'0x44'
         self.header_2 = b'0x77'
         self.u = 0
-        self.mode =0
-        #self.timer.timeout.connect(self.update_gps)
-    # GCS - STM32 Serial ( telemetry )     
+        self.mode_li = []
+        self.MISSION_LAT = 0
+        self.MISSION_LON = 0
+        self.automatic = 1
+        self.automatic_li = []
+        self.GPStime = []
+        # GCS - STM32 Serial ( telemetry )     
+        #self.ser = serial.Serial('COM5',115200,timeout=1)
+        #self.ser.flush()
+    def missionlatFunction(self):
+        self.MISSION_LAT = float(self.mission_lat.text())
+        print(self.MISSION_LAT)
+    def missionlonFunction(self):
+        self.MISSION_LON = float(self.mission_lon.text())
+        print(self.MISSION_LON)
+    def RTHlatFunction(self):
+        self.RTH_LAT = float(self.RTH_lat.text())
+        print(self.RTH_LAT)
+    def RTHlonFunction(self):
+        self.RTH_LON = float(self.RTH_lon.text())
+        print(self.RTH_LON)
     def connectSerial(self):
         self.ser = serial.Serial('COM6', 115200, timeout=1)
         self.ser.flush()
-    
+        self.btnSerial.setText("connect FC with Serial ")
     # GCS - NX Socket ( wifi )
     def connectSocket(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.HOST, self.port))
         self.timer.timeout.connect(self.update_gps)
-
+        self.btnSocket.setText("connect FCC with Socket ")
     # Socket을 통해 NX에서 GPS 데이터 받아온 후 저장 및 지도에 그리기    
     def update_gps(self):
         self.client_socket.sendall(self.NX_data)
         self.NX_data = self.client_socket.recv(1024)
         print(self.NX_data) # 값 들어오는거 확인용
-        self.mode , lat_drone , lon_drone , lat_person , lon_person , altitude  = self.NX_data.decode().split('\n')
+        mode , lat_drone , lon_drone , gps_time , lat_person , lon_person , altitude  = self.NX_data.decode().split('\n')
         print(self.rad) # 역시 확인용
-        self.lat_drone.append(lat_drone) ; self.lon_drone.append(lon_drone)
+        self.automatic_li.append(self.automatic)
+        self.mode_li.append(mode)  
+        self.lat_drone.append(lat_drone) ; self.lon_drone.append(lon_drone) ; self.GPStime.append(gps_time)
         self.lat_person.append(lat_person) ; self.lon_person.append(lon_person) 
         self.altitude.append(altitude) # 비행 데이터 제출용
 
         # 마킹코드
-        self.m = folium.Map(location=[37.5872530,127.0307692],  tiles='cartodbpositron', zoom_start=13)
-        folium.CircleMarker(color = 'red' , fill_color = 'red' ,location=[float(lat_drone),float(lon_drone)],radius=5 , popup="célula",).add_to(self.m)
-        folium.CircleMarker(color = 'blue' , fill_color = 'blue' ,location=[float(lat_person),float(lon_person)],radius=5 , popup="célula",).add_to(self.m)
-        self.data = io.BytesIO()
-        self.m.save(self.data, close_file=False)
-        self.webview2.setHtml(self.data.getvalue().decode())
+        if self.rad % 3 == 0 :
+            self.m = folium.Map(location=[37.5872530,127.0307692],  tiles='cartodbpositron', zoom_start=13)
+            folium.CircleMarker(color = 'red' , fill_color = 'red' ,location=[float(lat_drone),float(lon_drone)],radius=5 , popup="célula",).add_to(self.m)
+            folium.CircleMarker(color = 'blue' , fill_color = 'blue' ,location=[float(lat_person),float(lon_person)],radius=5 , popup="célula",).add_to(self.m)
+            self.data = io.BytesIO()
+            self.m.save(self.data, close_file=False)
+            self.webview2.setHtml(self.data.getvalue().decode())
 
         self.rad +=1
         self.altstatus.setText(f"고도 : {self.rad}m")
     # 이륙 명령 -> NX한테 이륙 명령 코드를 보낸다.
     def takeoff(self):
-
-        self.client_socket.sendall(b"11\n")
+        msgT = f"1\n{self.MISSION_LAT}\n{self.MISSION_LON}"
+        self.client_socket.sendall(msgT.encode())
         self.u = self.client_socket.recv(1024)
+    
     # 리턴투홈
-
     def RTH(self):
-        self.client_socket.sendall(b"66\n")
+        msgR = f"6\n{self.RTH_LAT}\n{self.RTH_LON}"
+        self.client_socket.sendall(msgR.encode())
         self.u = self.client_socket.recv(1024)
     # 강제종료
     def forceTerminate(self):
-
-        self.client_socket.sendall(b"99\n")
+        self.client_socket.sendall(b"9\n")
         self.u = self.client_socket.recv(1024)
-    # 데이터 저장
+    # 데이터 저장 
+    # 비행 데이터  : (자동 : 0 , 수동 : 1) / mode / Gps time / lat / lon / alt
     def datasave(self):
         df = pd.DataFrame()
+        df['automatic'] = self.automatic_li ; df['point'] = self.mode_li 
+        df['GPS Time'] = self.GPStime
         df['lat_drone'] = self.lat_drone ; df['lon_drone'] = self.lon_drone 
-        #df['lat_person'] = self.lat_person ; df['lon_person'] = self.lon_person
         df['altitude'] = self.altitude
-        df.to_csv("flight_data.csv")
+                #df['lat_person'] = self.lat_person ; df['lon_person'] = self.lon_person
+        now = time.localtime()
+        timevar = time.strftime('%Y%m%d%H%M%S', now)
+        df.to_csv(f"{timevar}_Flight_data.csv")
 
 app=QtWidgets.QApplication([])
 ex=window()
 sys.exit(app.exec_())
 
-import NX
-a = NX()
-a.connectST
