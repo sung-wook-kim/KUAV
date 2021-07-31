d02c5096223f5c2064d0db5b6b0fc949db9af0b4
jng <mine1372@korea.ac.kr>
2021-07-31 23:26:01 +0900

    add doublepid

--- AirSim/main.py
+++ AirSim/main.py
@@ -1,6 +1,8 @@
 import airsim
 from airsim import MultirotorClient
 
+import time
+
 class PIDSingle():
     def __init__(self):
         self.kp = None
@@ -8,19 +10,19 @@ class PIDSingle():
         self.kd = None
         self.reference = None
         self.meas_value = None
-        self.meas_value_prev = None
+        self.meas_value_prev = 0
         self.error = None
-        self.error_prev = None
-        self.error_sum = None
+        self.error_prev = 0
+        self.error_sum = 0
         self.error_deriv = None
-        self.error_deriv_filt = None
+        self.error_deriv_filt = 0
         self.p_result = None
         self.i_result = None
         self.d_result = None
         self.pid_result = None
 
 class PIDDouble():
-    def __init(self):
+    def __init__(self):
         self.IN = PIDSingle()
         self.OUT = PIDSingle()
 
@@ -33,8 +35,13 @@ class MyClient(MultirotorClient):
         self.client.confirmConnection()
         self.client.enableApiControl(True)
         self.client.armDisarm(True)
-        # client.reset() # 실행 시 뒤에 코드가 무시될 수 있어 필요할 때만 주석 해제
+        #self.client.reset() # 실행 시 뒤에 코드가 무시될 수 있어 필요할 때만 주석 해제
         self.client.takeoffAsync().join()
+
+        self.roll = PIDDouble()
+        self.pitch = PIDDouble()
+        # Set PID Gain
+        self.set_pid_gain()
 
         # Drone Value
         self.angle = {'pitch': None, 'roll': None, 'yaw': None}
@@ -42,10 +49,13 @@ class MyClient(MultirotorClient):
         self.altitude = None
         self.longitude = None
         self.latitude = None
-        self.motor = {1:0.60001,2:0.60001,3:0.6,4:0.6,'duration':1}
+        self.DT = 0.001
+
+        self.ccr = {1 : None, 2 : None, 3 : None, 4 : None, 'duration': 1}
 
     def run(self):
         while 1:
+            start = time.time()
             # Read Sensor Data
             # IMU
             self.read_angle()
@@ -55,20 +65,43 @@ class MyClient(MultirotorClient):
             # GPS
             self.read_gps()
 
+            # PID Control
+            self.Double_Roll_Pitch_PID_Calculation(self.pitch, 0, self.angle['pitch'], self.rate['pitch'], self.DT)
+            self.Double_Roll_Pitch_PID_Calculation(self.roll, 0, self.angle['roll'], self.rate['roll'], self.DT)
+
+            # PWM
+            self.ccr[1] = 84000 + 600 * 84 - self.pitch.IN.pid_result + self.roll.IN.pid_result
+            self.ccr[2] = 84000 + 600 * 84 + self.pitch.IN.pid_result + self.roll.IN.pid_result
+            self.ccr[3] = 84000 + 600 * 84 + self.pitch.IN.pid_result - self.roll.IN.pid_result
+            self.ccr[4] = 84000 + 600 * 84 - self.pitch.IN.pid_result - self.roll.IN.pid_result
+
+            for i in range(4):
+                if self.ccr[i+1] < 84000: self.ccr[i+1] = 84000
+                elif self.ccr[i+1] > 168000: self.ccr[i+1] = 168000
+
+                self.ccr[i+1] = (self.ccr[i+1] - 84000) / 84000
+
             # Write Motors
-            self.client.moveByMotorPWMsAsync(self.motor[4], self.motor[2],\
-                                             self.motor[1], self.motor[3], self.motor['duration']).join()
+            # print(self.ccr[1],self.ccr[2],self.ccr[3],self.ccr[4])
+            self.client.moveByMotorPWMsAsync(self.ccr[4], self.ccr[2],\
+                                             self.ccr[1], self.ccr[3], self.ccr['duration'])
 
             # print('roll : ',self.angle['roll'],' pitch : ', self.angle['pitch'], ' yaw : ', self.angle['yaw'])
+
+            self.DT = start - time.time()
 
     def read_angle(self):
         self.angle['pitch'], self.angle['roll'], self.angle['yaw'] = \
         airsim.to_eularian_angles(self.client.getImuData('Imu', 'Drone1').orientation)
 
+        self.angle['pitch'] *= -1
+
     def read_rate(self):
         self.rate['roll'] = self.client.getImuData('Imu', 'Drone1').angular_velocity.x_val
         self.rate['pitch'] = self.client.getImuData('Imu', 'Drone1').angular_velocity.y_val
         self.rate['yaw'] = self.client.getImuData('Imu', 'Drone1').angular_velocity.z_val
+
+        self.rate['pitch'] *= -1
 
     def read_altitude(self):
         self.altitude = self.client.getBarometerData(barometer_name='Barometer', vehicle_name='Drone1').altitude
@@ -83,7 +116,7 @@ class MyClient(MultirotorClient):
         axis.OUT.reference = set_point_angle
         axis.OUT.meas_value = angle
         axis.OUT.error = axis.OUT.reference - axis.OUT.meas_value
-        axis.OUT.p_result = axis.OUT.error*axis.OUT.kp
+        axis.OUT.p_result = axis.OUT.error * axis.OUT.kp
 
         # I calculation, DT need to be given in every loop
         axis.OUT.error_sum = axis.OUT.error_sum + axis.OUT.error * DT  # Define summation of outer loop
@@ -97,7 +130,7 @@ class MyClient(MultirotorClient):
         OUTER_DERIV_FILT_ENABLE = True
         axis.OUT.error_deriv = -rate  # Define derivative of outer loop (rate = ICM-20602 Angular Rate)
         if (not OUTER_DERIV_FILT_ENABLE) :
-            axis.OUT.d_result = axis.OUT.error_deriv * axis.OUT.kd;  # Calculate D result of outer loop
+            axis.OUT.d_result = axis.OUT.error_deriv * axis.OUT.kd  # Calculate D result of outer loop
         else :
             axis.OUT.error_deriv_filt = axis.OUT.error_deriv_filt * 0.4 + axis.OUT.error_deriv * 0.6 # filter for derivative
             axis.OUT.d_result = axis.OUT.error_deriv_filt * axis.OUT.kd  # Calculate D result of inner loop
@@ -107,7 +140,7 @@ class MyClient(MultirotorClient):
 
         # ************ Double PID Inner Begin (Roll and Pitch Angular Rate Control) **************#
         axis.IN.reference = axis.OUT.pid_result  # Set point of inner PID control is the PID result of outer loop (for double PID control)
-        axis.IN.meas_value = rate;  # ICM-20602 angular rate
+        axis.IN.meas_value = rate  # ICM-20602 angular rate
 
         axis.IN.error = axis.IN.reference - axis.IN.meas_value  # Define error of inner loop
         axis.IN.p_result = axis.IN.error * axis.IN.kp  # Calculate P result of inner loop
@@ -131,9 +164,22 @@ class MyClient(MultirotorClient):
 
         axis.IN.pid_result = axis.IN.p_result + axis.IN.i_result + axis.IN.d_result  # Calculate PID result of inner loop
         # ****************************************************************************************#
+    def set_pid_gain(self):
+        self.pitch.IN.kp = 104
+        self.pitch.IN.ki = 0
+        self.pitch.IN.kd = 7
+        self.pitch.OUT.kp = 14
+        self.pitch.OUT.ki = 0
+        self.pitch.OUT.kd = 3
 
+        self.roll.IN.kp = 104
+        self.roll.IN.ki = 0
+        self.roll.IN.kd = 7
+        self.roll.OUT.kp = 14
+        self.roll.OUT.ki = 0
+        self.roll.OUT.kd = 3
 
 if __name__ == "__main__":
-    kuav = MyClient()
 
+    kuav = MyClient()
     kuav.run()
