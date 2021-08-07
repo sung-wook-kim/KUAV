6c1165dbe469583b6674996ac9ef3bde61dbb732
impactWhey-KSW <plackyou123@korea.ac.kr>
2021-08-07 17:48:11 +0900

    add more value

--- STM32F4/Monitor_GPS_value.py
+++ STM32F4/Monitor_GPS_value.py
@@ -7,24 +7,8 @@ import random
 import time
 import pandas as pd
 
-# 네가지 값 프린트 , 전체변경으로 value 값을 원하는 변수로 변경 하세요
-# alt , target , error global declaration + value
-global alt , target , error ,value, d_result , voltage
-alt = 0
-target = 0
-error = 0
-value = 0
-d_result = 0
-voltage = 0
-
 ser = serial.Serial('COM7', 115200)
 ser.flush()
-
-#
-    # for save
-i = 0
-now = time.localtime()
-timevar = time.strftime('%d%H%M%S', now)
 
 while True:
     i+=1
@@ -33,8 +17,20 @@ while True:
     if a == 0x77:
         b = int(ser.read(1).hex(), 16)
         if b == 0x17:
-            num_sv = int(ser.read(1).hex(), 16) & 0xff
             
+            volt_1 = int(ser.read(1).hex(), 16) & 0xff
+            volt_2 = int(ser.read(1).hex(), 16) & 0xff
+            volt_3 = int(ser.read(1).hex(), 16) & 0xff
+            volt_4 = int(ser.read(1).hex(), 16) & 0xff
+
+            num_sv = int(ser.read(1).hex(), 16) & 0xff
+
+            bno080_yaw_1 = int(ser.read(1).hex(), 16) & 0xff
+            bno080_yaw_2 = int(ser.read(1).hex(), 16) & 0xff
+            bno080_yaw_3 = int(ser.read(1).hex(), 16) & 0xff
+            bno080_yaw_4 = int(ser.read(1).hex(), 16) & 0xff
+
+
             pvt_lat_1 = int(ser.read(1).hex(), 16) & 0xff
             pvt_lat_2 = int(ser.read(1).hex(), 16) & 0xff
             pvt_lat_3 = int(ser.read(1).hex(), 16) & 0xff
@@ -45,31 +41,44 @@ while True:
             pvt_lon_3 = int(ser.read(1).hex(), 16) & 0xff
             pvt_lon_4 = int(ser.read(1).hex(), 16) & 0xff
             
-            l_lat_1 = int(ser.read(1).hex(), 16) & 0xff
-            l_lat_2 = int(ser.read(1).hex(), 16) & 0xff
-            l_lat_3 = int(ser.read(1).hex(), 16) & 0xff
-            l_lat_4 = int(ser.read(1).hex(), 16) & 0xff
+            target_lat_1 = int(ser.read(1).hex(), 16) & 0xff
+            target_lat_2 = int(ser.read(1).hex(), 16) & 0xff
+            target_lat_3 = int(ser.read(1).hex(), 16) & 0xff
+            target_lat_4 = int(ser.read(1).hex(), 16) & 0xff
 
-            l_lon_1 = int(ser.read(1).hex(), 16) & 0xff
-            l_lon_2 = int(ser.read(1).hex(), 16) & 0xff
-            l_lon_3 = int(ser.read(1).hex(), 16) & 0xff
-            l_lon_4 = int(ser.read(1).hex(), 16) & 0xff
+            target_lon_1 = int(ser.read(1).hex(), 16) & 0xff
+            target_lon_2 = int(ser.read(1).hex(), 16) & 0xff
+            target_lon_3 = int(ser.read(1).hex(), 16) & 0xff
+            target_lon_4 = int(ser.read(1).hex(), 16) & 0xff
 
-            bno080_yaw_1 = int(ser.read(1).hex(), 16) & 0xff
-            bno080_yaw_2 = int(ser.read(1).hex(), 16) & 0xff
-            bno080_yaw_3 = int(ser.read(1).hex(), 16) & 0xff
-            bno080_yaw_4 = int(ser.read(1).hex(), 16) & 0xff
+            pitch_adjust_1 = int(ser.read(1).hex(), 16) & 0xff
+            pitch_adjust_2 = int(ser.read(1).hex(), 16) & 0xff
+            pitch_adjust_3 = int(ser.read(1).hex(), 16) & 0xff
+            pitch_adjust_4 = int(ser.read(1).hex(), 16) & 0xff
 
+            roll_adjust_1 = int(ser.read(1).hex(), 16) & 0xff
+            roll_adjust_2 = int(ser.read(1).hex(), 16) & 0xff
+            roll_adjust_3 = int(ser.read(1).hex(), 16) & 0xff
+            roll_adjust_4 = int(ser.read(1).hex(), 16) & 0xff
+
+            pitch_sign = pitch_adjust_1 >> 7
+            roll_sign = roll_adjust_1 >> 7
             yaw_sign = bno080_yaw_1 >> 7
             
+            voltage = volt_1 << 24 | volt_2 << 16 | volt_3 << 8 | volt_4
+
             pvt_lat = pvt_lat_1 << 24 | pvt_lat_2 << 16 | pvt_lat_3 << 8 | pvt_lat_4
             pvt_lon = pvt_lon_1 << 24 | pvt_lon_2 << 16 | pvt_lon_3 << 8 | pvt_lon_4
-            l_lat = l_lat_1 << 24 | l_lat_2 << 16 | l_lat_3 << 8 | l_lat_4
-            l_lon = l_lon_1 << 24 | l_lon_2 << 16 | l_lon_3 << 8 | l_lon_4
-            bno080_yaw = bno080_yaw_1 << 24 | bno080_yaw_2 << 16 | bno080_yaw_3 << 8 | bno080_yaw
+
+            target_lat = target_lat_1 << 24 | target_lat_2 << 16 | target_lat_3 << 8 | target_lat_4
+            target_lon = target_lon_1 << 24 | target_lon_2 << 16 | target_lon_3 << 8 | target_lon_4
+            
+            bno080_yaw = bno080_yaw_1 << 24 | bno080_yaw_2 << 16 | bno080_yaw_3 << 8 | bno080_yaw_4
 
             if yaw_sign == 1: bno080_yaw = (bno080_yaw & 0x7fffffff) - 2 ** 31
-    
-            print(num_sv, pvt_lat, pvt_lon , l_lat, l_lon)
+            if pitch_sign == 1: pitch_adjust = (pitch_adjust & 0x7fffffff) - 2 ** 31
+            if roll_sign == 1: roll_adjust = (roll_adjust & 0x7fffffff) - 2 ** 31
+
+            print(voltage, num_sv,  bno080_yaw , pvt_lat , pvt_lon , target_lat , target_lon , pitch_adjust , roll_adjust )
 
 
