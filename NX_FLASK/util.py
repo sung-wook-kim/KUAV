8c328fa8085b38a273b8b279445c20fc861334cd
seungwoo <tmddn833@korea.ac.kr>
2021-08-31 09:51:03 +0900

    gimbaltest

--- NX_FLASK/util.py
+++ NX_FLASK/util.py
@@ -5,6 +5,39 @@ This file is written for drone control (calculate rotation matrix, geo calculati
 import math
 import numpy as np
 
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1920,
+    display_height=1080,
+    framerate=60,
+    flip_method=0,
+    
+    # capture_width=3264,
+    # capture_height=2464,
+    # display_width=3264,
+    # display_height=2464,
+    # framerate=21,
+    # flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 def get_intersections(cen0, r0, cen1, r1):
     # circle 1: (x0, y0), radius r0
